bf70a9a3b894ff0580c12bd018864b2e2e28757a
n451 <2020200706@ruc.edu.cn>
2023-11-10 21:06:02 +0800

    hmm performance ver1

--- lua/hmm.lua
+++ lua/hmm.lua
@@ -15,49 +15,56 @@ local PrevStatus = {
 }
 
 local function viterbi(obs, states, start_p, trans_p, emit_p)
-	local V = { {} } -- tabular
-	local path = {}
-	for _, y in pairs(states) do -- init
-		V[1][y] = start_p[y] + (emit_p[y][obs[1]] or MIN_FLOAT)
-		path[y] = { y }
-	end
-	for t = 2, #obs do
-		V[t] = {}
-		local newpath = {}
-		for _, y in pairs(states) do
-			local em_p = (emit_p[y][obs[t]] or MIN_FLOAT)
-			local prob, state = nil, PrevStatus[y][1]
-			local max_prob = MIN_FLOAT
-			for _, y0 in pairs(PrevStatus[y]) do
-				local tr_p = trans_p[y0][y] or MIN_FLOAT
-				local prob0 = V[t - 1][y0] + tr_p + em_p
-				if prob0 > max_prob then
-					max_prob = prob0
-					state = y0
-				end
-			end
-			prob = max_prob
-			V[t][y] = prob
-			newpath[y] = {}
-			for _, p in pairs(path[state]) do
-				table.insert(newpath[y], p)
-			end
-			table.insert(newpath[y], y)
-		end
-		path = newpath
-	end
+    local V = { {} } -- tabular
+    local prev_best_state = {} -- optimized space usage
+    
+    for _, y in pairs(states) do -- init
+        V[1][y] = start_p[y] + (emit_p[y][obs[1]] or MIN_FLOAT)
+        prev_best_state[y] = {}
+    end
+    
+    for t = 2, #obs do
+        V[t] = {}
+        for _, y in pairs(states) do
+            local em_p = (emit_p[y][obs[t]] or MIN_FLOAT)
+            local max_prob = MIN_FLOAT
+            local best_prev_state
+            
+            for _, y0 in pairs(states) do
+                local tr_p = trans_p[y0][y] or MIN_FLOAT
+                local prob0 = V[t - 1][y0] + tr_p + em_p
+                if prob0 > max_prob then
+                    max_prob = prob0
+                    best_prev_state = y0
+                end
+            end
+            
+            V[t][y] = max_prob
+            prev_best_state[y][t] = best_prev_state
+        end
+    end
 
-	local prob, state = nil, "E"
-	local max_prob = MIN_FLOAT
-	for _, y in pairs({ "E", "S" }) do
-		if V[#obs][y] > max_prob then
-			max_prob = V[#obs][y]
-			state = y
-		end
-	end
-	prob = max_prob
+    -- Find the most probable final state
+    local max_prob = MIN_FLOAT
+    local best_final_state
 
-	return prob, path[state]
+    for _, y in pairs(states) do
+        if V[#obs][y] > max_prob then
+            max_prob = V[#obs][y]
+            best_final_state = y
+        end
+    end
+
+    -- Build and return the most probable path
+    local most_probable_path = {best_final_state}
+    local current_best_state = best_final_state
+
+    for t = #obs, 2, -1 do
+        current_best_state = prev_best_state[current_best_state][t]
+        table.insert(most_probable_path, 1, current_best_state)
+    end
+
+    return most_probable_path
 end
 
 local function cut(sentence, start_p, trans_p, emit_p)
@@ -66,7 +73,7 @@ local function cut(sentence, start_p, trans_p, emit_p)
 	for i in string.gmatch(sentence, "[%z\1-\127\194-\244][\128-\191]*") do
 		table.insert(s_res, i)
 	end
-	local prob, pos_list = viterbi(s_res, { "B", "M", "E", "S" }, start_p, trans_p, emit_p)
+	local pos_list = viterbi(s_res, { "B", "M", "E", "S" }, start_p, trans_p, emit_p)
 	local result = {}
 	local begin, nexti = 1, 1
 	local sentence_length = #s_res
@@ -81,40 +88,22 @@ local function cut(sentence, start_p, trans_p, emit_p)
 				table.insert(res, v)
 			end
 			local val = table.concat(res)
-			coroutine.yield(val)
-			table.insert(result, val)
+      result[#result] = val
 			nexti = i + 1
 		elseif pos == "S" then
-			coroutine.yield(char)
 			result[#result + 1] = char
 			nexti = i + 1
 		end
 	end
 	if nexti <= sentence_length then
-		table.insert(result, s_res[nexti])
-		coroutine.yield(s_res[nexti])
+    result[#result] = s_res[nexti]
 	end
 	return result
 end
 
-local _cut = function(sentence)
-	local co = coroutine.create(function()
-		cut(sentence, start, trans, emit)
-	end)
-	return function()
-		local _, res = coroutine.resume(co)
-		return res
-	end
-end
-
 M.lcut = function(sentence)
-	local res = {}
-	for i in _cut(sentence) do
-		table.insert(res, i)
-	end
-	return res
+  return cut(sentence,  start, trans, emit)
 end
-
 -- local Force_Split_Words = {}
 -- function contains(table, val)
 --    for _, value in ipairs(table) do
@@ -130,9 +119,13 @@ function M.cut(sentence)
 	local result = {}
 	for _, blk in ipairs(blocks) do
 		if ut.isAllChinese(blk) then
-			for _, word in ipairs(M.lcut(blk)) do
-				result[#result + 1] = word
-			end
+      local l = M.lcut(blk)
+      for _, word in pairs(l) do
+        result[#result + 1] = word
+      end
+			-- for _, word in ipairs(M.lcut(blk)) do
+			-- 	result[#result + 1] = word
+			-- end
 		else
 			for _, word in ipairs(ut.splitString(blk)) do
 				result[#result + 1] = word
@@ -141,5 +134,9 @@ function M.cut(sentence)
 	end
 	return result
 end
-
+-- local t = os.clock()
+-- for i =1, 100000 do
+--   M.cut("韩冰是个")
+-- end
+-- print(os.clock() -t)
 return M
